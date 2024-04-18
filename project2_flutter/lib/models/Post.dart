64edea1d67601dd241471df0b8969e3eda7b900a
AzmiSadath <163077111+AzmiSadath@users.noreply.github.com>
2024-04-18 14:42:19 -0400

    Added Post.dart File

---
+++ project2_flutter/lib/models/Post.dart
@@ -0,0 +1,17 @@
+class Post {
+  final int id;
+  final String userName;
+  final double connectionType;
+  final String description;
+  final String image;
+  final String dateTime;
+
+  Post({
+    required this.id,
+    required this.userName,
+    required this.connectionType,
+    required this.description,
+    required this.image,
+    required this.dateTime
+  });
+}
